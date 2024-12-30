1ad69e12bccfc419f29e0717b867f3c6a164fc71
= <=>
2024-12-30 18:24:49 -0300

    create SpringDataJPA dependency

---
+++ src/main/java/dev/buskopan/dependencies/impl/SpringDataJPA.java
@@ -0,0 +1,22 @@
+package dev.buskopan.dependencies.impl;
+
+import dev.buskopan.dependencies.BaseIDependency;
+
+public class SpringDataJPA extends BaseIDependency {
+
+    private static final String VERSION = "3.4.1";
+
+    @Override
+    public String getMavenDependency() {
+        return "<dependency>\n" +
+                "    <groupId>org.springframework.boot</groupId>\n" +
+                "    <artifactId>spring-boot-starter-data-jpa</artifactId>\n" +
+                "    <version>"+VERSION+"</version>\n" +
+                "</dependency>\n";
+    }
+
+    @Override
+    public String getGradleDependency() {
+        return "implementation group: 'org.springframework.boot', name: 'spring-boot-starter-data-jpa', version: '"+VERSION+"'\n";
+    }
+}
